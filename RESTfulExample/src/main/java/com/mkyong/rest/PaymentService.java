242f4820cb3aed9babd141031df6c85a46a64f0c
gargsandeep <sandeepg4202@gmail.com>
2016-02-28 16:33:27 +0530

    Added files via upload

---
+++ RESTfulExample/src/main/java/com/mkyong/rest/PaymentService.java
@@ -0,0 +1,30 @@
+package com.mkyong.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.mkyong.transaction.TransactionBo;
+
+//@Component
+//@Service
+@Path("/payment")
+public class PaymentService {
+
+	@Autowired
+	TransactionBo transactionBo;
+
+	@GET
+	@Path("mkyong")
+	public Response savePayment() {
+
+		String result = transactionBo.save();
+
+		return Response.status(200).entity(result).build();
+
+	}
+
+}
---
+++ RESTfulExample/src/main/java/com/mkyong/transaction/impl/TransactionBoImpl.java
@@ -0,0 +1,14 @@
+package com.mkyong.transaction.impl;
+
+import com.mkyong.transaction.TransactionBo;
+
+
+public class TransactionBoImpl implements TransactionBo {
+
+	public String save() {
+
+		return "Jersey + Spring example by sandeep";
+
+	}
+
+}
